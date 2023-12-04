502abbb218d5e577b2dc44ef71dd99738c86158a
V_A_N <V_A_N-1_94@mail.ru>
2023-12-04 20:54:32 +0300

    fix code

--- src/logger_config.py
+++ src/logger_config.py
@@ -1,10 +1,13 @@
 import logging
+from pathlib import Path
 
 
 def setup_logging():
     """Конфикурация logging"""
+    ROOT_PATH = Path(__file__).parent.parent
+    filename = ROOT_PATH.joinpath("data", "log_file.txt")
     logging.basicConfig(
-        filename="log_file.txt",
+        filename=filename,
         filemode="w",
         encoding="UTF-8",
         level=logging.INFO,
--- src/main.py
+++ src/main.py
@@ -2,18 +2,29 @@ import os
 from datetime import datetime
 from pathlib import Path
 
+from dotenv import load_dotenv
+
 from src.logger_config import setup_logging
-from src.reports import spending_by_category, save_file
+from src.reports import spending_by_category
 from src.services import simple_search
 from src.utils import get_transactions, get_user_settings
-from src.views import greetings, get_num_card, get_top_transactions, get_exchange_rate, get_stock_prices, get_page_main
-from dotenv import load_dotenv
+from src.views import (
+    get_exchange_rate,
+    get_num_card,
+    get_page_main,
+    get_stock_prices,
+    get_top_transactions,
+    greetings,
+)
+
 load_dotenv()
 
 logger = setup_logging()
 
+ROOT_PATH = Path(__file__).parent.parent
+
+
 def main() -> None:
-    ROOT_PATH = Path(__file__).parent.parent
     OPERATIONS_JSON = ROOT_PATH.joinpath("data", "operations.xls")
 
     USER_SETTINGS = ROOT_PATH.joinpath("user_settings.json")
@@ -34,8 +45,9 @@ def main() -> None:
     stock_prices = get_stock_prices(user_stocks, api_key)
 
     get_page_main(greeting, num_card, top_transactions, exchange_rate, stock_prices)
-    simple_search(transactions,'Супермаркеты')
+    simple_search(transactions, 'Супермаркеты')
     spending_by_category(transactions, category, time_)
+
 
 if __name__ == '__main__':
     main()
